d1263020a9b57b2dabaa0a8b7d69667559a56731
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-07-14 10:30:03 -0700

    v1.3.0.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Change Log
 
+### v1.3.0
+
+* Updated for Meteor 1.4 by upgrading kadira-binary-deps for 1.5.0
+
 ### v1.2.1
 
 * Add random and check packages to support Meteor 1.2
--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   "summary": "CPU Profiler for Meteor (used with Kadira)",
-  "version": "1.2.1",
+  "version": "1.3.0",
   "git": "https://github.com/meteorhacks/kadira-profiler.git",
   "name": "meteorhacks:kadira-profiler"
 });
@@ -18,13 +18,13 @@ Package.onTest(function(api) {
 });
 
 function configurePackage(api) {
-  api.versionsFrom('METEOR@1.0');
+  api.versionsFrom('METEOR@1.4-beta.7');
   api.use('http');
   api.use('check');
   api.use('random');
   api.use('meteorhacks:kadira@2.22.0');
   api.imply('meteorhacks:kadira@2.22.0');
-  api.use('meteorhacks:kadira-binary-deps@1.4.0');
+  api.use('meteorhacks:kadira-binary-deps@1.5.0');
 
   api.add_files('lib/server.js', 'server');
   api.add_files('lib/client.js', 'client');
