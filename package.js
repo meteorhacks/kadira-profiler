f341fbdd7752a0b824ff846af014c99503c1f859
buithanhbavuong <bavuongco10@gmail.com>
2018-10-26 20:18:51 +0700

    feat: upgrade

--- package.js
+++ package.js
@@ -1,8 +1,7 @@
 Package.describe({
   "summary": "CPU Profiler for Meteor (used with Kadira)",
-  "version": "1.3.0",
-  "git": "https://github.com/meteorhacks/kadira-profiler.git",
-  "name": "meteorhacks:kadira-profiler"
+  "version": "1.4.0",
+  "name": "bavuongco10:kadira-profiler"
 });
 
 Package.onUse(function(api) {
@@ -22,9 +21,9 @@ function configurePackage(api) {
   api.use('http');
   api.use('check');
   api.use('random');
-  api.use('meteorhacks:kadira@2.22.0');
-  api.imply('meteorhacks:kadira@2.22.0');
-  api.use('meteorhacks:kadira-binary-deps@1.5.0');
+  api.use('meteorhacks:kadira');
+  api.imply('meteorhacks:kadira');
+  api.use('bavuongco10:kadira-binary-deps');
 
   api.add_files('lib/server.js', 'server');
   api.add_files('lib/client.js', 'client');
